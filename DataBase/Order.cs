4afdf8f9a1bdfa3478674e8e75327d9c9eb9f687
Garik Frost <DreamJob666@outlook.com>
2023-12-01 20:15:09 +0300

    Add model Order

---
+++ DataBase/Order.cs
@@ -0,0 +1,18 @@
+﻿// Зависимая сущность
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DataBase
+{
+    internal class Order
+    {
+        public Guid OrderId { get; set; } // Номер заказа
+
+        [Required]
+        public string infoOrder { get; set; } = null!; // Информация о заказе
+
+        public int UserId { get; set; } // внешний ключ
+        public User? User { get; set; } // навигацонное свойство
+    }
+}
--- DataBase/User.cs
+++ DataBase/User.cs
@@ -1,12 +1,17 @@
-﻿// Объекты, которые будут храниться в базе данных
+﻿// Главная сущность
+using System.ComponentModel.DataAnnotations;
+
 namespace DataBase
 {
     public class User
     {
         public Guid id {  get; set; }
-        public string? firstName { get; set; } = null!; 
-        public string? email { get; set; } = null!;
-        public string? password { get; set; } = null!; 
-        public string? gender { get; set; } = null!;
+        [Required]
+        public string firstName { get; set; } = null!;
+        [Required]
+        public string email { get; set; } = null!;
+        [Required]
+        public string password { get; set; } = null!; 
+        public string gender { get; set; } = null!;
     }
 }
