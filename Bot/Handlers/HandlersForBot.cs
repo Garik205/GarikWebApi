b34dded8b2ceb8708c6812fd576459cec8293119
Garik Frost <DreamJob666@outlook.com>
2023-12-06 19:36:16 +0300

    Add TelegramBot

---
+++ Bot/Handlers/HandlersForBot.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+
+namespace Bot.Handlers
+{
+    public class HandlersForBot
+    {
+        protected static Task ErrorHandler(ITelegramBotClient telegramBotClient, Exception error, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = error switch  // поле для хранения кода ошибки и её сообщение
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        protected static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try // обработчик ошибок, чтобы бот не лёг в случае ошибок
+            {
+                switch (update.Type) // для обработки Update
+                {
+                    case UpdateType.Message:
+                        {
+                            Console.WriteLine("Пришло новое сообщение.");
+                            return;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,40 @@
+﻿using Bot.Handlers;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot
+{
+    class Program : HandlersForBot
+    {
+        private static ITelegramBotClient? _botClient; // Клиент для работы с Telegram Bot Api (Позволяет отправлять сообщения, управлять ботом, подписываться на обновления и прочее)
+
+        private static ReceiverOptions? _receiverOptions; // Объект с настройками работы бота. Позволяет указывать какие типы Update мы будем получать, Timeout бота и прочее
+
+        static async Task Main() 
+        {
+            _botClient = new TelegramBotClient("6543089403:AAG0KkWyvB8LIbX7BhQB1S9Vpl9MoSpYdz4"); // Наш токен полученный от бота
+
+            _receiverOptions = new ReceiverOptions() // Присваемваем значение для настройки бота
+            {
+                AllowedUpdates = new[] // Тип получаемых Update
+                {
+                    UpdateType.Message, // Можем получить сообщения(текст, фото и другую инфу) 
+                },
+                ThrowPendingUpdates = true, // параметр, который отвечает за обработку полученных сообщений во время нахожения бота в offline режиме. True - не надо обрабатывать, False - надо обрабатывать.
+            };
+
+            using var cts = new CancellationTokenSource();
+
+            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
+            // UpdateHandler - обработчик приходящих Update
+            // ErrorHandler - обработчик ошибок для Bot API
+
+            var me = await _botClient.GetMeAsync(); // поле для хранения информации о моём боте
+
+            Console.WriteLine($"{me.FirstName} мой бот запущен ");
+
+            await Task.Delay(-1);
+        }
+    }
+}
